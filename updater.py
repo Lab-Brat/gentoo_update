1eedc8a9d85c01b1f1b76d59824083cf422f9f45
Lab-Brat <xoovoox@protonmail.com>
2023-05-15 21:55:53 +0300

    use subprocess instead of os module

--- updater.py
+++ updater.py
@@ -1,17 +1,21 @@
 import os
-
+import subprocess
 
 current_path = os.path.dirname(os.path.realpath(__file__))
 
-def update_safe_mode():
+def run_bash_script(script_path):
     try:
-        os.system(f"{current_path}/bash_scripts/glsa-check.sh")
-        # Continue processing the result...
-    except RuntimeError as e:
-        print(e)
-        # Handle the error...
+        result = subprocess.run(['bash', script_path], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        print(f'[Script Output] >> {result.stdout.decode()}')
+    except subprocess.CalledProcessError as e:
+        print(f'[Error] >>>>>>>>>> {script_path} exited with error code {e.returncode}]')
+        print(f'[Error Message] >> {e.stderr.decode()}')
+
 
 def updater():
-    os.system(f"{current_path}/bash_scripts/dependencies.sh")
-    update_safe_mode()
+    # ensure dependencies are installed
+    run_bash_script(f"{current_path}/bash_scripts/dependencies.sh")
+
+    # Updater Safe Mode: Update GLSA only
+    run_bash_script(f"{current_path}/bash_scripts/glsa-check.sh")
 
