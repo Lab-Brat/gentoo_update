e192d03973c691c81f9bec66eb11c662966fd528
Lab-Brat <xoovoox@protonmail.com>
2023-05-15 11:32:08 +0300

    add some structure

---
+++ main.py
@@ -0,0 +1,4 @@
+from updater import updater
+
+updater()
+
--- updater.py
+++ updater.py
@@ -1,7 +1,17 @@
 import os
 
+
 current_path = os.path.dirname(os.path.realpath(__file__))
 
-# Step 1: Ensure that dependencies are installed
-os.system(f"{current_path}/bash_scripts/dependencies.sh")
+def update_safe_mode():
+    try:
+        os.system(f"{current_path}/bash_scripts/glsa-check.sh")
+        # Continue processing the result...
+    except RuntimeError as e:
+        print(e)
+        # Handle the error...
+
+def updater():
+    os.system(f"{current_path}/bash_scripts/dependencies.sh")
+    update_safe_mode()
 
