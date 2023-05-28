118cd0bcb2b3a5e636d47b42899a05f07cb7a6d6
Lab-Brat <xoovoox@protonmail.com>
2023-05-28 15:50:02 +0300

    change script launch from bash -> sh

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import subprocess
 current_path = os.path.dirname(os.path.realpath(__file__))
 
 
-def run_bash_script(script_path, *args):
+def run_shell_script(script_path, *args):
     try:
         result = subprocess.run(
-            ### Not all users will have bash - can we default to `sh`?
-            ["bash", script_path] + list(args),
+            ### [done] Not all users will have bash - can we default to `sh`?
+            ["sh", script_path] + list(args),
             check=True,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
@@ -32,7 +32,7 @@ DAEMON_RESTART = "true"
 
 # Run the updater
 ### Since you are running the updater from python, you should create the commandline options flags with help documentation in python and pass them all into the shell script
-run_bash_script(
+run_shell_script(
     f"{current_path}/updater.sh",
     UPGRADE_MODE,
     CONFIG_UPDATE_MODE,
