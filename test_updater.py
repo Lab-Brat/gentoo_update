3231d9c745724132379a95a0dc796d2eb58e3333
Lab-Brat <xoovoox@protonmail.com>
2023-06-08 23:58:11 +0300

    format code

--- test_updater.py
+++ test_updater.py
@@ -4,33 +4,34 @@ from gentoo_update import create_logger, add_prefixes, run_shell_script
 
 
 class TestGentooUpdate(unittest.TestCase):
-
-    @mock.patch('gentoo_update.os.path.exists', return_value=False)
-    @mock.patch('gentoo_update.os.mkdir')
+    @mock.patch("gentoo_update.os.path.exists", return_value=False)
+    @mock.patch("gentoo_update.os.mkdir")
     def test_create_logger(self, mock_mkdir, mock_exists):
         logger, log_file = create_logger()
         self.assertTrue(mock_mkdir.called)
         self.assertEqual(logger.level, 20)
 
     def test_add_prefixes(self):
-        input_args = ['a', 'bc']
-        expected_output = ['-a', '--bc']
+        input_args = ["a", "bc"]
+        expected_output = ["-a", "--bc"]
         self.assertEqual(add_prefixes(input_args), expected_output)
 
-    @mock.patch('gentoo_update.subprocess.Popen')
-    @mock.patch('gentoo_update.create_logger', return_value=(mock.Mock(), 'log_file'))
-    @mock.patch('gentoo_update.shlex.split')
+    @mock.patch("gentoo_update.subprocess.Popen")
+    @mock.patch(
+        "gentoo_update.create_logger", return_value=(mock.Mock(), "log_file")
+    )
+    @mock.patch("gentoo_update.shlex.split")
     def test_run_shell_script(self, mock_split, mock_logger, mock_popen):
         # mocking the split, logger and popen calls
         mock_process = mock.Mock()
         mock_process.returncode = 0
-        mock_process.stdout = [b'test output\n']
-        mock_process.stderr = [b'test error\n']
+        mock_process.stdout = [b"test output\n"]
+        mock_process.stderr = [b"test error\n"]
         mock_popen.return_value.__enter__.return_value = mock_process
-        run_shell_script('arg1', 'arg2')
+        run_shell_script("arg1", "arg2")
         self.assertTrue(mock_split.called)
         self.assertTrue(mock_popen.called)
 
-if __name__ == '__main__':
-    unittest.main()
 
+if __name__ == "__main__":
+    unittest.main()
