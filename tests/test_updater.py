bb4f2ea57cea09aaa9972dd7428287fd5c600c1c
Lab-Brat <xoovoox@protonmail.com>
2023-06-10 00:14:22 +0300

    black formatting

--- tests/test_updater.py
+++ tests/test_updater.py
@@ -4,7 +4,9 @@ from gentoo_update import create_logger, add_prefixes, run_shell_script
 
 
 class TestGentooUpdate(unittest.TestCase):
-    @mock.patch("gentoo_update.gentoo_update.os.path.exists", return_value=False)
+    @mock.patch(
+        "gentoo_update.gentoo_update.os.path.exists", return_value=False
+    )
     @mock.patch("gentoo_update.gentoo_update.os.mkdir")
     def test_create_logger(self, mock_mkdir, _):
         logger, _ = create_logger()
@@ -18,7 +20,8 @@ class TestGentooUpdate(unittest.TestCase):
 
     @mock.patch("gentoo_update.gentoo_update.subprocess.Popen")
     @mock.patch(
-        "gentoo_update.gentoo_update.create_logger", return_value=(mock.Mock(), "log_file")
+        "gentoo_update.gentoo_update.create_logger",
+        return_value=(mock.Mock(), "log_file"),
     )
     @mock.patch("gentoo_update.gentoo_update.shlex.split")
     def test_run_shell_script(self, mock_split, _, mock_popen):
