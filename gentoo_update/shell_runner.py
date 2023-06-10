2a73e79556784661ccb160ff365bbff3a5440f85
Lab-Brat <xoovoox@protonmail.com>
2023-06-10 16:22:05 +0300

    move stream output processing to _log_stream_output()

--- gentoo_update/shell_runner.py
+++ gentoo_update/shell_runner.py
@@ -5,6 +5,7 @@ import logging
 import subprocess
 from datetime import datetime
 
+
 class ShellRunner:
     def __init__(self):
         self.timestamp = datetime.now().strftime("%Y-%m-%d-%H-%M")
@@ -20,40 +21,66 @@ class ShellRunner:
         Both handlers have the same logging level (INFO)
         and share the same formatter.
         Formatters include timestamp, log level and the message.
-    
+
         Returns:
             logging.Logger: Configured logger.
             log_filename: Log filename.
         """
         if not os.path.exists(self.log_dir):
             os.mkdir(self.log_dir)
-    
+
         logger = logging.getLogger(__name__)
         logger.setLevel(logging.INFO)
-    
+
         terminal_handler = logging.StreamHandler()
         terminal_handler.setLevel(logging.INFO)
         file_handler = logging.FileHandler(self.log_filename)
         file_handler.setLevel(logging.INFO)
-    
+
         formater = logging.Formatter(
             "[%(asctime)s %(levelname)s] ::: %(message)s",
             datefmt="%d-%b-%y %H:%M:%S",
         )
         terminal_handler.setFormatter(formater)
         file_handler.setFormatter(formater)
-    
+
         logger.addHandler(terminal_handler)
         logger.addHandler(file_handler)
-    
+
         return logger
 
+    def _log_stream_output(self, stream_obj, type):
+        """
+        Process sterr from the upadte script.
+
+        Args:
+            stream: output stream from subprocess.Popen
+            type: output type, stderr or stdout
+        Returns:
+            List[str]: List containing the output
+        """
+        output = []
+        if type == "stdout":
+            stream = stream_obj.stdout
+        elif type == "stderr":
+            stream = stream_obj.stderr
+        else:
+            self.logger.error("Invalid Output Stream Type")
+
+        for line in stream:
+            line = line.decode().rstrip("\n")
+            output.append(line)
+            if type == "stdout":
+                self.logger.info(line)
+            elif type == "stderr":
+                self.logger.error(line)
+        return output
 
     def run_shell_script(self, *args):
         """
         Run a shell script and stream standard output
         and standard error to terminal and a log file.
-    
+
         Args:
             script_path (str): Shell script path.
             *args (str): Arguments for the shell script.
@@ -63,23 +90,12 @@ class ShellRunner:
         with subprocess.Popen(
             command, stdout=subprocess.PIPE, stderr=subprocess.PIPE
         ) as script_stream:
-            # Process stdout
-            for line in script_stream.stdout:
-                self.logger.info(line.decode().rstrip("\n"))
-    
-            # Process stderr
-            stderr_output = []
-            for line in script_stream.stderr:
-                line = line.decode().rstrip("\n")
-                stderr_output.append(line)
-                self.logger.error(line)
-    
+            stdout_output = self._log_stream_output(script_stream, "stdout")
+            stderr_output = self._log_stream_output(script_stream, "stderr")
             script_stream.wait()
-    
+
             if script_stream.returncode != 0:
-                error_message = (
-                    "updater.sh exited with error code {script_stream.returncode}"
-                )
+                error_message = "updater.sh exited with error code {script_stream.returncode}"
                 if stderr_output:
                     stderr_output_message = "n".join(stderr_output)
                     error_message += (
