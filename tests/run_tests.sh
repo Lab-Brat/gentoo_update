2cd56dc868264a0c4aea9b5607c2fd1977fa5e69
Lab-Brat <xoovoox@protonmail.com>
2023-06-14 21:34:54 +0300

    add set -e and improve indentations

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
+set -euo pipefail
+
 emerge_install() {
-    # Install from GURU repository
-    echo "app-admin/gentoo_update ~amd64" >> /etc/portage/package.accept_keywords/gentoo_update
-    emerge --quiet-build y app-admin/gentoo_update
+	# Install from GURU repository
+	echo "app-admin/gentoo_update ~amd64" >>/etc/portage/package.accept_keywords/gentoo_update
+	emerge --quiet-build y app-admin/gentoo_update
 }
 
 pip_install() {
-    # Install with pip
-    cd /root/gentoo_update_source && pip install . --break-system-packages
+	# Install with pip
+	cd /root/gentoo_update_source && pip install . --break-system-packages
 }
 
 sec_update() {
-    # Run the gentoo-update in secure mode
-    gentoo-update
+	# Run the gentoo-update in secure mode
+	gentoo-update
 }
 
 full_update() {
-    # Run the gentoo-update in full update mode
-    gentoo-update -m full
+	# Run the gentoo-update in full update mode
+	gentoo-update -m full
 }
 
 "$@"
-
