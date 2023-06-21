d9991ac224b742ad404326de86c2b47a52e10680
Lab-Brat <xoovoox@protonmail.com>
2023-06-21 15:31:06 +0300

    change indentation in Bash scripts to 4 spaces

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -3,34 +3,34 @@
 set -euo pipefail
 
 emerge_install() {
-	# Install from GURU repository
-	echo "app-admin/gentoo_update ~amd64" >>/etc/portage/package.accept_keywords/gentoo_update
-	emerge --quiet-build y app-admin/gentoo_update
+    # Install from GURU repository
+    echo "app-admin/gentoo_update ~amd64" >>/etc/portage/package.accept_keywords/gentoo_update
+    emerge --quiet-build y app-admin/gentoo_update
 }
 
 pip_install() {
-	# Install with pip
-	cd /root/gentoo_update_source && pip install . --break-system-packages
+    # Install with pip
+    cd /root/gentoo_update_source && pip install . --break-system-packages
 }
 
 sec_update() {
-	# Run the gentoo-update in secure mode
-	gentoo-update
+    # Run the gentoo-update in secure mode
+    gentoo-update
 }
 
 full_update() {
-	# Run the gentoo-update in full update mode
-	gentoo-update --update-mode full
+    # Run the gentoo-update in full update mode
+    gentoo-update --update-mode full
 }
 
 full_update_all_options() {
-	# Run the gentoo-update in full update mode and all options enabled
-	gentoo-update --update-mode full \
-		--config-update-mode merge \
-		--daemon-restart y \
-		--clean y \
-		--read-logs y \
-		--read-news y
+    # Run the gentoo-update in full update mode and all options enabled
+    gentoo-update --update-mode full \
+        --config-update-mode merge \
+        --daemon-restart y \
+        --clean y \
+        --read-logs y \
+        --read-news y
 }
 
 "$@"
