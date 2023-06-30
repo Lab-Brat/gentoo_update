336bb7b57643140bd323ea30f0c49f7fc4492f98
Lab-Brat <xoovoox@protonmail.com>
2023-06-30 09:33:38 +0300

    use regular expression to match

--- gentoo_update/parser.py
+++ gentoo_update/parser.py
@@ -1,6 +1,6 @@
+import re
 import pprint
 from typing import Dict, List
-
 
 class Parser:
     def __init__(self, log_file: str) -> None:
@@ -20,23 +20,26 @@ class Parser:
         Returns:
             List[str]: The content of the log file as a list of strings.
         """
-        with open(self.log_file, "r") as f:
-            log_data = f.readlines()
-        return log_data
+        with open(self.log_file, "r") as log_file:
+            log_data = log_file.readlines()
+        return self.split_log_to_sections(log_data)
 
-    def split_log_to_sections(self) -> Dict[str, List[str]]:
+    def split_log_to_sections(self, log_data: List) -> Dict[str, List[str]]:
         """
         Splits the log file into sections based on specified markers.
 
         Returns:
             Dict[str, List[str]]: A dictionary with section names as keys and content as values.
         """
+        section_pattern = r"\{\{(.+?)\}\}"
+
         section_name = "beginning"
         log_by_sections: Dict[str, List[str]] = {}
-        for line in self.log_data:
+        for line in log_data:
             if " ::: " in line:
-                line = line.split(" ::: ")[1]
-                if line.startswith("{{"):
+                line = line.split(" ::: ")[1].strip()
+                match_section_pattern = re.search(section_pattern, line)
+                if match_section_pattern:
                     section_name = line
                     log_by_sections[section_name] = []
                 else:
@@ -49,5 +52,4 @@ class Parser:
 
 if __name__ == "__main__":
     parser = Parser("./log_for_tests")
-    sections = parser.split_log_to_sections()
-    pprint.pprint(sections)
+    pprint.pprint(parser.log_data["{{ UPDATE SYSTEM }}"])
