3ed7c61c41f3d56ff87afc70b0e00ecefe5adfaa
Lab-Brat <xoovoox@protonmail.com>
2023-06-30 23:10:10 +0300

    add parsing structure for "pretend" and "update" sections

--- gentoo_update/parser.py
+++ gentoo_update/parser.py
@@ -1,6 +1,8 @@
 import re
+import sys
 import pprint
 from typing import Dict, List
+
 
 class Parser:
     def __init__(self, log_file: str) -> None:
@@ -12,6 +14,7 @@ class Parser:
         """
         self.log_file = log_file
         self.log_data = self.read_log()
+        self.split_sections()
 
     def read_log(self) -> List[str]:
         """
@@ -29,12 +32,13 @@ class Parser:
         Splits the log file into sections based on specified markers.
 
         Returns:
-            Dict[str, List[str]]: A dictionary with section names as keys and content as values.
+            Dict[str, List[str]]: A dictionary with section names as
+                                  keys and content as values.
         """
         section_pattern = r"\{\{(.+?)\}\}"
-
         section_name = "beginning"
-        log_by_sections: Dict[str, List[str]] = {}
+        log_by_sections: Dict[str, List[str]] = {section_name: []}
+
         for line in log_data:
             if " ::: " in line:
                 line = line.split(" ::: ")[1].strip()
@@ -49,7 +53,35 @@ class Parser:
 
         return log_by_sections
 
+    def split_sections(self):
+        for section in self.log_data.keys():
+            section_content = self.log_data[section]
+            if section == "{{ PRETEND EMERGE }}":
+                self.split_emerge_pretend_section(section_content)
+            elif section == "{{ UPDATE SYSTEM }}":
+                self.split_update_system_section(section_content)
+        print("Section parsing finished!")
+
+    def split_emerge_pretend_section(self, section_content: List[str]):
+        pretend_successful = "emerge pretend was successful, updating..." in section_content
+        if pretend_successful:
+            print("Pretend completed without errors")
+        else:
+            print("Pretend exited with errors: ")
+            print("Error 1: ....")
+            print("Error 2: ....")
+
+    def split_update_system_section(self, section_content: List[str]):
+        update_type = section_content[1].split()[1]
+        if update_type == "@world":
+            pass
+        elif update_type == "GLSA":
+            pass
+        else:
+            print(f"{update_type} is a wrong update type")
+            print("Correct types: @world and GLSA")
+            sys.exit(0)
+
 
 if __name__ == "__main__":
     parser = Parser("./log_for_tests")
-    pprint.pprint(parser.log_data["{{ UPDATE SYSTEM }}"])
