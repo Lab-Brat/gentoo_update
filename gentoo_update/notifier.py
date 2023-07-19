cc423c550592eee94223d883a010baa0ef9aab2b
Lab-Brat <xoovoox@protonmail.com>
2023-07-19 23:30:54 +0300

    add a POC notifier that sends messages to IRC chat

---
+++ gentoo_update/notifier.py
@@ -0,0 +1,77 @@
+import os
+import socket
+import ssl
+import time
+from typing import Dict
+
+
+class Notifier:
+    def __init__(self, notification_type: str, report: Dict) -> None:
+        if notification_type == "email":
+            pass
+        elif notification_type == "irc":
+            server = "irc.libera.chat"
+            port = 6697
+            channel = os.environ["irc_chan"]
+            botnick = os.environ["irc_nick"]
+            botpassword = os.environ["irc_pass"]
+            self.send_report_to_itc(server, port, channel, botnick, botpassword)
+
+    def send_report_to_itc(
+        self,
+        server: str,
+        port: int,
+        channel: str,
+        botnick: str,
+        botpassword: str,
+    ) -> None:
+        """
+        Send the update report to IRC chat.
+        """
+        irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        ssl_context = ssl.create_default_context()
+        irc = ssl_context.wrap_socket(irc, server_hostname=server)
+
+        irc.connect((server, port))
+
+        # Send the necessary commands to identify the bot and join the channel
+        irc.send(
+            bytes(
+                "USER "
+                + botnick
+                + " "
+                + botnick
+                + " "
+                + botnick
+                + " "
+                + botnick
+                + "\n",
+                "UTF-8",
+            )
+        )
+        irc.send(bytes("NICK " + botnick + "\n", "UTF-8"))
+        irc.send(
+            bytes(
+                "PRIVMSG NickServ :IDENTIFY "
+                + botnick
+                + " "
+                + botpassword
+                + "\n",
+                "UTF-8",
+            )
+        )
+        time.sleep(10)
+
+        irc.send(bytes("JOIN " + channel + "\n", "UTF-8"))
+
+        message = "I AM CROCUBOT"
+        irc.send(bytes("PRIVMSG " + channel + " :" + message + "\n", "UTF-8"))
+
+        time.sleep(30)
+
+        irc.send(bytes("QUIT \n", "UTF-8"))
+        irc.close()
+
+
+if __name__ == "__main__":
+    notify = Notifier(notification_type="irc", report={})
