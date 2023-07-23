c2d4120c1581cc49451280e0c61f62e36c4addf0
Lab-Brat <xoovoox@protonmail.com>
2023-07-23 21:41:42 +0300

    add notifier.py to init.py

--- gentoo_update/__init__.py
+++ gentoo_update/__init__.py
@@ -2,4 +2,5 @@ from .gentoo_update import add_prefixes, create_cli, main
 from .shell_runner import ShellRunner
 from .parser import Parser
 from .reporter import Reporter
+from .notifier import Notifier
 from ._version import __version__
