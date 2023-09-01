231246cbdd84988261cab7975a818bd362ffed79
Lab-Brat <xoovoox@protonmail.com>
2023-09-01 16:53:22 +0300

    rewrite Docker test script to be more portable

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -2,40 +2,78 @@
 
 set -euo pipefail
 
-emerge_install() {
-    # Install from GURU repository
-    echo "app-admin/gentoo_update ~amd64" >>/etc/portage/package.accept_keywords/gentoo_update
-    emerge --quiet-build y app-admin/gentoo_update
+# ---------------------- VARIABLES ----------------------- #
+INSTALL_METHOD="${1}"
+UPDATE_MODE="${2}"
+SHOW_REPORT="${3:-n}"
+SEND_REPORT="${4:-n}"
+
+# --------------------- TEST SCRIPT ---------------------- #
+install_gentoo_update() {
+    # Install ebuild from GURU repository
+    if [[ "${INSTALL_METHOD}" == 'ebuild' ]]; then
+        echo "app-admin/gentoo_update ~amd64" >>/etc/portage/package.accept_keywords/gentoo_update
+        emerge --quiet-build y app-admin/gentoo_update
+
+    # Install python package from PyPi
+    elif [[ "${INSTALL_METHOD}" == 'pip' ]]; then
+        pip install gentoo_update --break-system-packages
+
+    # Install python pacakge from source
+    elif [[ "${INSTALL_METHOD}" == 'source' ]]; then
+        cd /root/gentoo_update_source && pip install . --break-system-packages
+
+    else
+        echo "Invalid installation method"
+        exit 1
+
+    fi
 }
 
-pip_install() {
-    # Install with pip
-    cd /root/gentoo_update_source && pip install . --break-system-packages
+run_gentoo_update() {
+    # Update GLSA only
+    if [[ "${UPDATE_MODE}" == 'security' ]]; then
+        gentoo-update
+
+    # Update @world
+    elif [[ "${UPDATE_MODE}" == 'full' ]]; then
+        gentoo-update --update-mode full
+
+    # Update @world with all flags
+    elif [[ "${UPDATE_MODE}" == 'full_with_all_opts' ]]; then
+        gentoo-update --update-mode full \
+            --config-update-mode merge \
+            --daemon-restart \
+            --clean \
+            --read-logs \
+            --read-news
+
+    else
+        echo "Invalid update mode"
+        exit 1
+
+    fi
 }
 
-pypi_install() {
-    # Install with pip from PyPi
-    pip install gentoo_update --break-system-packages
+show_update_report() {
+    # print the update report
+    if [[ "${SHOW_REPORT}" == 'y' ]]; then
+        gentoo-update --report
+    else
+        echo "Not printing report"
+    fi
 }
 
-sec_update() {
-    # Run the gentoo-update in secure mode
-    gentoo-update
+send_report() {
+    # send report via irc, email or mobile
+    if [[ "${SEND_REPORT}" != 'n' ]]; then
+        gentoo-update --send-report "${SEND_REPORT}"
+    else
+        echo "Not sending report"
+    fi
 }
 
-full_update() {
-    # Run the gentoo-update in full update mode
-    gentoo-update --update-mode full
-}
-
-full_update_all_options() {
-    # Run the gentoo-update in full update mode and all options enabled
-    gentoo-update --update-mode full \
-        --config-update-mode merge \
-        --daemon-restart \
-        --clean \
-        --read-logs \
-        --read-news
-}
-
-"$@"
+install_gentoo_update
+run_gentoo_update
+show_update_report
+send_report
