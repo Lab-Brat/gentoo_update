675e3bb3ad0444d06606d76d00d18fbcf623a267
Mark Gomersbach <markgomersbach@gmail.com>
2023-10-03 01:31:58 +0200

    Disable F401 only for __init__.py Also isort has been applied

--- gentoo_update/__init__.py
+++ gentoo_update/__init__.py
@@ -1,6 +1,6 @@
+from ._version import __version__
 from .gentoo_update import add_prefixes, create_cli, main
-from .shell_runner import ShellRunner
+from .notifier import Notifier
 from .parser import Parser
 from .reporter import Reporter
-from .notifier import Notifier
-from ._version import __version__
+from .shell_runner import ShellRunner
