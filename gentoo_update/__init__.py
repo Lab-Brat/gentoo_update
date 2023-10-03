8d0f28df866253edbdf791ad57effa00f2a35158
Mark Gomersbach <markgomersbach@gmail.com>
2023-10-03 02:08:33 +0200

    Add file docstring for module for D104

--- gentoo_update/__init__.py
+++ gentoo_update/__init__.py
@@ -1,3 +1,15 @@
+"""
+This module provides functionality for updating a Gentoo Linux system.
+
+It includes the following submodules:
+- _version: contains the version number of the package
+- gentoo_update: contains the main functionality for updating the system
+- notifier: provides functionality for sending notifications
+- parser: provides functionality for parsing command line arguments
+- reporter: provides functionality for generating reports
+- shell_runner: provides functionality for running shell commands
+"""
+
 from ._version import __version__
 from .gentoo_update import add_prefixes, create_cli, main
 from .notifier import Notifier
