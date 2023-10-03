ffb065391bdf2ca82fbd1ccf98aae2fea226cded
Mark Gomersbach <markgomersbach@gmail.com>
2023-10-03 03:49:46 +0200

    Update some docstrings and consolidate style.
    Catch more override in loops.
    Replace 202 magic number

--- gentoo_update/__init__.py
+++ gentoo_update/__init__.py
@@ -1,14 +1,4 @@
-"""
-This module provides functionality for updating a Gentoo Linux system.
-
-It includes the following submodules:
-- _version: contains the version number of the package
-- gentoo_update: contains the main functionality for updating the system
-- notifier: provides functionality for sending notifications
-- parser: provides functionality for parsing command line arguments
-- reporter: provides functionality for generating reports
-- shell_runner: provides functionality for running shell commands
-"""
+"""Provides functionality for updating a Gentoo Linux system."""
 
 from ._version import __version__
 from .gentoo_update import add_prefixes, create_cli, main
