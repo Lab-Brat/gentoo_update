3e18511f217daf0019d89e9bf2b23d7bba9659cb
Lab-Brat <xoovoox@protonmail.com>
2023-10-05 23:16:00 +0300

    move package specific information parsing to parser_package.py

--- gentoo_update/__init__.py
+++ gentoo_update/__init__.py
@@ -4,6 +4,7 @@ from ._version import __version__
 from .gentoo_update import add_prefixes, create_cli, main
 from .notifier import Notifier
 from .parser import Parser
+from .parser_package import PackageParser
 from .report_objects import (
     DiskUsage,
     DiskUsageStats,
