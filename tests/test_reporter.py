15e9f562b195f5f8cb3337aa2f8a800d337f2b44
Lab-Brat <xoovoox@protonmail.com>
2024-01-18 00:27:07 +0300

    add tests for 2nd report

--- tests/test_reporter.py
+++ tests/test_reporter.py
@@ -8,6 +8,7 @@ from gentoo_update.gentoo_update import (
     get_available_log_files,
 )
 from gentoo_update.reporter import Reporter
+from _report_packages_2 import report_2_packages
 
 
 class TestGentooUpdate(unittest.TestCase):
@@ -18,18 +19,28 @@ class TestGentooUpdate(unittest.TestCase):
         test_reporter_path = path.dirname(path.abspath(__file__))
         logs_for_tests = f"{test_reporter_path}/logs_for_unit_tests"
         log_1, log_2 = get_available_log_files(logs_for_tests, 2)
-        self.report_object_1 = generate_report(
-            logs_for_tests, log_1, short_report=False
-        )
+
+        self.report_object_1 = generate_report(logs_for_tests, log_1)
+        self.report_object_2 = generate_report(logs_for_tests, log_2)
         self.report_1 = self.report_object_1.create_report()
+        self.report_2 = self.report_object_2.create_report()
 
     def test_report_object_type_1(self):
         """Test if the report_object is of type Reporter."""
         self.assertIs(type(self.report_object_1), Reporter)
 
+    def test_report_object_type_2(self):
+        """Test if the report_object is of type Reporter."""
+        self.assertIs(type(self.report_object_2), Reporter)
+
     def test_report_status_1(self):
         """Test if the report status is SUCCESS."""
         status = self.report_1[1].split(" ")[-1]
+        self.assertEqual(status, "SUCCESS")
+
+    def test_report_status_2(self):
+        """Test if the report status is SUCCESS."""
+        status = self.report_2[1].split(" ")[-1]
         self.assertEqual(status, "SUCCESS")
 
     def test_report_packages_1(self):
@@ -43,6 +54,12 @@ class TestGentooUpdate(unittest.TestCase):
         ]
         self.assertEqual(packages, correct_package_list)
 
+    def test_report_packages_2(self):
+        """Test if the report lists the expected packages."""
+        packages = self.report_2[2:-5]
+        correct_package_list = report_2_packages
+        self.assertEqual(packages, correct_package_list)
+
     def test_disk_usage_1(self):
         """Test if the report shows the expected disk usage."""
         disk_usage = self.report_1[-4::]
@@ -53,6 +70,18 @@ class TestGentooUpdate(unittest.TestCase):
             "Used (%) 42% => 42%",
         ]
         self.assertEqual(disk_usage, correct_disk_usage)
+
+    def test_disk_usage_2(self):
+        """Test if the report shows the expected disk usage."""
+        disk_usage = self.report_2[-4::]
+        correct_disk_usage = [
+            'Mount Point /',
+            'Free Space 260G => 257G',
+            'Used Space 170G => 173G',
+            'Used (%) 40% => 41%'
+        ]
+        self.assertEqual(disk_usage, correct_disk_usage)
+
 
 
 if __name__ == "__main__":
